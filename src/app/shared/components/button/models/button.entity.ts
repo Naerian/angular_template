57a4840fef31a9ec81a0d657dd36d9ef90eb4b64
Ángel Jiménez <angel.jimenez@neoris.com>
2024-03-20 16:33:34 +0100

    Añadidos colores de boton de estado y mejoras de estilos

--- src/app/shared/components/button/models/button.entity.ts
+++ src/app/shared/components/button/models/button.entity.ts
@@ -1,4 +1,4 @@
 export type ButtonSize = 'xs' | 's' | 'xm' | 'm' | 'l' | 'xl';
 export type ButtonType = 'submit' | 'reset' | 'button';
-export type ButtonColor = 'primary' | 'secondary';
+export type ButtonColor = 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'info';
 export type ButtonMode = 'button' | 'icon';
